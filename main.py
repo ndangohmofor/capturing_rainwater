5967fa5daf81d781a6dd19272645a95e28a79e1f
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-12 06:34:14 -0500

    Code the optimized solution

--- main.py
+++ main.py
@@ -16,6 +16,25 @@ def naive_solution(heights):
 
     return total_water
 
+
+def optimized_solution(heights):
+    total_water = 0
+    left_pointer = 0
+    right_pointer = len(heights) - 1
+    left_bound = 0
+    right_bound = 0
+
+    while left_pointer < right_pointer:
+        if heights[left_pointer] <= heights[right_pointer]:
+            left_bound = max(left_bound, heights[left_pointer])
+            total_water += left_bound - heights[left_pointer]
+            left_pointer += 1
+        else:
+            right_bound = max(right_bound, heights[right_pointer])
+            total_water += right_bound - heights[right_pointer]
+            right_pointer -= 1
+    return total_water
+
 # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
 #     print(naive_solution())
